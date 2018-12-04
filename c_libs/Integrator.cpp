1d76e5728e379a0400b0f017f0874476c96b613b
Lucas Hintze <lucas.hintze@fh-muenster.de>
2018-12-04 14:29:15 +0100

    simpson integral

--- c_libs/Integrator.cpp
+++ c_libs/Integrator.cpp
@@ -6,6 +6,33 @@
  */
 #include "Integrator.h"
 
+double getXj(double a, double h, double j) {
+	return a + (h * j);
+}
+
+double shortenedSimpsonIntegral(Function& f, double a, double b, double n) {
+	double factor = (b-a)/(6*n);
+	double fa = f(a);
+	double fb = f(b);
+	double sum1;
+	double sum2;
+	double h = (b - a) / n;
+
+	for (int j = 1; j <= n-1; j++) {
+		sum1 += f(getXj(a, h, j));
+	}
+	sum1 *= 2;
+
+	for (int j = 0; j <= n-1; j++) {
+		sum2 += f((getXj(a, h, j) + getXj(a, h, j+1))/2);
+	}
+	sum2 *= 4;
+
+	return factor * (fa + fb + sum1 + sum2);
+}
+
+
+
 double integrate(Function& f, double a, double b, double eps) {
 
 	double test = f(2);
